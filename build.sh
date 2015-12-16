8c92b371c690c43dd23af13a2d70274111180475
Dmitry Kunin <dkunin@avito.ru>
2015-12-16 13:57:35 +0300

    Minor fix to build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ node -pe 'JSON.parse(process.argv[1])["version"]' "$(cat ./package.json)" > pack
 node -pe 'JSON.parse(process.argv[1])["name"]' "$(cat ./package.json)" > packname
 rm -rf ./dist
 npm run build
-cd ./dist && zip -r -X $(cat ../packname)-$(cat ../packver)-(git symbolic-ref -q HEAD | cut -d'/' -f 3).zip ./*
+cd ./dist && zip -r -X $(cat ../packname)-$(cat ../packver)-$(git symbolic-ref -q HEAD | cut -d'/' -f 3).zip ./*
