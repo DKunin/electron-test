3835061bab3540b0b97a774873b898148c351a89
Dmitry Kunin <dkunin@avito.ru>
2015-12-24 17:42:48 +0300

    Fix folder name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 BRANCH=$(git symbolic-ref -q HEAD | cut -d'/' -f 3)
-cp ./questons/$BRANCH/q-bank.js ./publictest/script
+cp ./questions/$BRANCH/q-bank.js ./publictest/script
 node -pe 'JSON.parse(process.argv[1])["version"]' "$(cat ./package.json)" > packver
 node -pe 'JSON.parse(process.argv[1])["name"]' "$(cat ./package.json)" > packname
 FILENAME=$(cat ./packname)-$(cat ./packver)-$BRANCH.zip
