41d9da26e426f5e5cb81b7a67bca2d32e0a40a8e
Dmitry Kunin <dkunin@avito.ru>
2015-12-24 17:41:28 +0300

    Update packageversion and build script

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ BRANCH=$(git symbolic-ref -q HEAD | cut -d'/' -f 3)
 cp ./questons/$BRANCH/q-bank.js ./publictest/script
 node -pe 'JSON.parse(process.argv[1])["version"]' "$(cat ./package.json)" > packver
 node -pe 'JSON.parse(process.argv[1])["name"]' "$(cat ./package.json)" > packname
+FILENAME=$(cat ./packname)-$(cat ./packver)-$BRANCH.zip
 rm -rf ./dist
 npm run build
-cd ./dist && zip -r -X $(cat ../packname)-$(cat ../packver)-$BRANCH.zip ./*
+cd ./dist && zip -r -X $FILENAME ./*
+mv $FILENAME ../../
